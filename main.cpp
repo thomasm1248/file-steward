c8179d9e18b3dcd9d6530d3b43d5d72e36ecc412
thomasm1248 <thomasm1248@gmail.com>
2023-07-12 10:46:23 -0600

    Start working on archive folder manager

--- main.cpp
+++ main.cpp
@@ -8,15 +8,23 @@
 
 namespace fs = std::filesystem;
 
+// Globals
 const int SECONDS_UNTIL_FILE_EXPIRATION = 60 * 60 * 24 * 30; // 30 days
 
-enum ruleType {tempFolder, backupFolder};
-
+// Types for managing rules
+enum ruleType {tempFolder, archiveFolder};
 struct rule
 {
     std::string path;
     ruleType type;
     int param;
+};
+
+// Types for managing archive inventory
+struct inventoryEntry
+{
+    std::string path;
+    std::string date;
 };
 
 void checkForExpiredFiles(fs::path path, int maxAge)
@@ -33,6 +41,70 @@ void checkForExpiredFiles(fs::path path, int maxAge)
                     fs::remove_all(subPath);
                 }
             } );
+}
+
+void zipModifiedSubfolders(fs::path archiveRoot)
+{
+    // read inventory file
+    std::vector<inventoryEntry> oldInventoryState;
+    std::ifstream inventoryFile{archiveRoot.string() + "/.steward"};
+    if (inventoryFile.is_open())
+    {
+        // Read each line of the file
+        std::string line;
+        while (std::getline(inventoryFile, line))
+        {
+            // Parse the line
+            std::string date;
+            std::string path;
+            enum {DATE, PATH} readingState = DATE;
+            for (char &c : line)
+            {
+                switch(readingState)
+                {
+                    case DATE:
+                        if (c != ' ')
+                            date += c;
+                        else
+                            readingState = PATH;
+                        break;
+                    case PATH:
+                        path += c;
+                        break;
+                }
+            }
+
+            // Create another inventory entry, and add to list
+            inventoryEntry newEntry{};
+            newEntry.path = path;
+            newEntry.date = date;
+            oldInventoryState.push_back(newEntry);
+            std::cout << oldInventoryState.size() << '\n';
+        }
+    }
+
+    // Scan archive and make package list
+    std::vector<inventoryEntry> newInventoryState;
+    std::ranges::for_each( fs::directory_iterator{archiveRoot},
+            [&newInventoryState](const auto& subPath) {
+                // Get last modified date
+                fs::file_time_type modTime = fs::last_write_time(subPath);
+                // Convert date to string (uuuuuugh)
+                auto sctp = std::chrono::time_point_cast<std::chrono::system_clock::duration>(modTime - std::chrono::file_clock::now() + std::chrono::system_clock::now());
+                std::time_t tt = std::chrono::system_clock::to_time_t(sctp);
+                std::string dateString = std::to_string(tt);
+                // Create new inventory entry, and add to list
+                inventoryEntry newEntry;
+                newEntry.path = subPath.path().string();
+                newInventoryState.push_back(newEntry);
+            } );
+    std::cout << newInventoryState.size() << '\n';
+
+    // Merge inventory data with package list
+
+    // Zip packages with recent modifications
+
+    // Generate new inventory file
 }
 
 int main(int argc, char** argv)
@@ -52,9 +124,9 @@ int main(int argc, char** argv)
     for (int i = 0; i < rulesAsText.size(); i++)
     {
         // Read the rule character by character
-        std::string command{""};
-        std::string param{""};
-        std::string path{""};
+        std::string command;
+        std::string param;
+        std::string path;
         enum {COMMAND, PARAM, PATH} readingState = COMMAND;
         for (char &c : rulesAsText.at(i))
         {
@@ -93,16 +165,16 @@ int main(int argc, char** argv)
             newRule.type = tempFolder;
             newRule.param = std::stoi(param);
         }
-        else if (command == "backupFolder")
+        else if (command == "archiveFolder")
         {
-            newRule.type = backupFolder;
+            newRule.type = archiveFolder;
         }
         else
         {
             std::cout << "Not a valid command: \"" << command << "\"\n";
             return 0;
         }
-        newRule.path = fs::path(path);
+        newRule.path = path;
         rules.push_back(newRule);
     }
 
@@ -114,8 +186,8 @@ int main(int argc, char** argv)
             case tempFolder:
                 checkForExpiredFiles(rules.at(i).path, rules.at(i).param);
                 break;
-            case backupFolder:
-                // Unimplemented todo implement
+            case archiveFolder:
+                zipModifiedSubfolders(rules.at(i).path);
                 break;
         }
     }
